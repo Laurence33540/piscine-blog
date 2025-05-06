3ecaf0c0a75ae6cbb67d00e75da4a8e6f44462bf
Laurence Renevier <Laurence.renevier@Lapiscine.pro>
2025-05-06 14:39:55 +0200

    formulaire_piscine_blog

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryForm;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityManager;
@@ -25,12 +26,38 @@ class CategoryController extends AbstractController {
 
         $category = $categoryRepository->find($id);
 
-        if(!$category) {
-
-            return $this->redirectToRoute('404');
-        }
-
         return $this->render('details-category.html.twig', ['category' => $category]);
 
     }
+
+	#[Route('/create-category', name: "create-category")]
+	public function displayCreateCategory(Request $request, EntityManagerInterface $entityManager) {
+
+		// je créé une instance de category
+		$category = new Category();
+
+		// je créé le formulaire 
+		// en utilisant le gabarit de formulaire "CategoryForm" généré avec "make:form"
+		// et l'instance de category
+		$categoryForm = $this->createForm(CategoryForm::class, $category);
+
+		// je stocke dans la variable du formulaire les données envoyées en POST
+		$categoryForm->handleRequest($request);
+
+		// je regarde s'il y a bien des données envoyées en POST
+		if ($categoryForm->isSubmitted()) {
+			// si oui, je sauvegarde la category
+			// dont les propriétés ont été automatiquement remplies 
+			// par symfony et le système de formulaire
+			$entityManager->persist($category);
+			$entityManager->flush();
+		}
+
+
+		return $this->render('create-category.html.twig', [
+			'categoryForm' => $categoryForm->createView()
+		]);
+
+	}
+
 }
---
+++ src/Form/CategoryForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Title')
+            ->add('Description')
+            ->add('Color')
+            ->add('CreateAt')
+            ->add('Submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
